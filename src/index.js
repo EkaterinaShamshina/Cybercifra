f5c7d2794683253fcd6a2aac178a1fc1f1280196
EkaterinaShamshina <nireta2009@yandex.ru>
2024-03-20 16:20:46 +0300

    feat: add up button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'normalize.css'
 import './styles/main.scss'
 
+//Smooth scroll
 const anchors = document.querySelectorAll('a[href*="#"]')
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
@@ -14,3 +15,36 @@ for (let anchor of anchors) {
     })
   })
 }
+
+//Up button
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  show() {
+    // удалим у кнопки класс btn-up_hide
+    this.el.classList.remove('btn-up_hide')
+  },
+  hide() {
+    // добавим к кнопке класс btn-up_hide
+    this.el.classList.add('btn-up_hide')
+  },
+  addEventListener() {
+    // при прокрутке содержимого страницы
+    window.addEventListener('scroll', () => {
+      // определяем величину прокрутки
+      const scrollY = window.scrollY || document.documentElement.scrollTop
+      // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+      scrollY > 400 ? this.show() : this.hide()
+    })
+    // при нажатии на кнопку .btn-up
+    document.querySelector('.btn-up').onclick = () => {
+      // переместим в начало страницы
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      })
+    }
+  },
+}
+
+btnUp.addEventListener()
