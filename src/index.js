35af22b1c8d9400c348cd680bb20fa19daebc0db
EkaterinaShamshina <nireta2009@yandex.ru>
2024-03-20 16:02:43 +0300

    feat: change font-size for h2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import 'normalize.css'
 import './styles/main.scss'
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  })
+}
